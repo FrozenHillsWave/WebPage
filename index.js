73678f76c038e92ea467ca63e4874ddf29a7eae3
FrozenHillsWave <91672060+FrozenHillsWave@users.noreply.github.com>
2021-10-11 20:16:37 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ function minimalize() {
     alert("Ablak minimalizálva!");
   }
 
+document.addEventListener("keydown", function () {
+  commands()
+});
+
 function commands() {
     var command = document.getElementById("commandprompt_input").value;
-    //document.getElementById("output").innerHTML = "Your answer is " + command;
     switch (command) {
         case "help":
             document.getElementById("output").innerHTML = "Használható parancsok:<br> \
@@ -28,4 +31,26 @@ function commands() {
             document.getElementById("output").innerHTML = 'Error! Please enter valid command! To see what commands you can use type "help" in to the command line';
             break;
     }
+}
+
+window.onload = function() {
+  HexGenerator()
+
+  document.getElementById("card0").style.backgroundColor = RobertColor;
+  document.getElementById("card1").style.backgroundColor = RobertColor;
+  document.getElementById("card2").style.backgroundColor = RobertColor;
+  document.getElementById("card3").style.backgroundColor = RobertColor;
+}
+
+
+function HexGenerator() {
+  HEXHolder = [];
+  for (let index = 0; index < 6; index++) {
+    HEX = Math.floor(Math.random() * (9 - 1 + 1) + 1);
+    HEXHolder.push(HEX.toString())
+  };
+  RobertColor = "#"
+  for (let index = 0; index < HEXHolder.length; index++) {
+    RobertColor += HEXHolder[index]
+  }
 }
